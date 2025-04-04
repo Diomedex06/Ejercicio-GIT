2c5e742dd0916552d19e0e8a54b3828192b494ea
Lulopartes <soporte@lulopartes.com>
2025-04-04 13:34:34 -0500

    Correccion PR

--- ejercicio_git/src/main/java/co/edu/etitc/sistemas/programacion4/Main.java
+++ ejercicio_git/src/main/java/co/edu/etitc/sistemas/programacion4/Main.java
@@ -1,10 +1,12 @@
 package co.edu.etitc.sistemas.programacion4;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Main {
     public static void main(String[] args) {
-        var contexto = new AnnotationConfigApplicationContext();
+        var contexto = new AnnotationConfigApplicationContext(Main.class);
         contexto.register(HolaMundo.class);
         contexto.refresh();
         var instancia = contexto.getBean(HolaMundo.class);
